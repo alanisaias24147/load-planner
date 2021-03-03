d55f4076068eb29c31f01b39f5956cce2e68baf4
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-03-03 17:17:52 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace load__planner
+{
+    public class Class1
+    {
+        //propiedades
+        string nombre;
+        int peso;
+        int altura;
+
+        //instrucciones
+        public static void abrebiaturaBarril()
+        {
+            Console.WriteLine("B");
+        }
+        public static void abrebiaturaBolsachatarra()
+        {
+            Console.WriteLine("BC");
+        }
+        public static void abrebiaturaBolsaplantas()
+        {
+            Console.WriteLine("BP");
+        }
+        public static void abrebiaturaCajaligera()
+        {
+            Console.WriteLine("C1");
+        }
+        public static void abrebiaturacajapesada()
+        {
+            Console.WriteLine("C2");
+        }
+        public static void abrebiaturaCajavacia()
+        {
+            Console.WriteLine("C3");
+        }
+        public static void abrebiaturaSilla()
+        {
+            Console.WriteLine("S");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace load__planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int[,] vehiculo = new int[4, 3];
+        
+         
+     }
+      
+         
+ }
+        
+    
+}
